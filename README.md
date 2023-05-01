5335bd011f95278550e0d5d3503986e0b680b704
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-01 20:11:20 +0200

    phonebook backend 3.11

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Full Stack Open - Part 3 exercises solutions
 This is a repository for part 3 of the [Full Stack Open course](https://fullstackopen.com/en/part3)
+
+Link to live version: [Phonebook](https://phonebook-backend-vhfd.onrender.com/)
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('persondata', function(req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :persondata'))
