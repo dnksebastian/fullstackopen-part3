a1a15d61ab268bbf5b7ab8c9b524994538abf665
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-02 21:00:30 +0200

    phonebook backend 3.12

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+
+const url =
+  `mongodb+srv://fullstackphonebook:${password}@phonebookdb.gub1r3w.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length < 3) {
+  console.log("Enter password as argument");
+  process.exit(1);
+} else if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const personName = process.argv[3];
+  const personNumber = process.argv[4];
+
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (process.argv.length > 5) {
+  console.log(
+    "Enter password only for phonebook entries list. Enter password, name and number to add a new person to the phonebook. If name or number contain whitespace, wrap them in quotes"
+  );
+  process.exit(1);
+}
