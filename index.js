f287a8239d0971aeded9b2d47d1c41d20cb68cea
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-03 18:09:33 +0200

    phonebook backend 3.13

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Imports
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-
 const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+
+const Person = require('./models/person')
 
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(express.static('build'))
 
 morgan.token('persondata', function(req, res) {return JSON.stringify(req.body)})
@@ -15,28 +17,30 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 
 // Functions and data
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
+
+// let persons = []
 
 const generateRandomId = (range) => {
     return Math.floor(Math.random() * range);
@@ -54,9 +58,9 @@ const checkIfUniqueName = (arg) => {
 
 // Routes
 
-app.get('/', (request, response) => {
-  response.send('<h1>Phonebook backend</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Phonebook backend</h1>')
+// })
 
 app.get('/info', (request, response) => {
     let currentDate = new Date();
@@ -65,7 +69,10 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  // response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -118,7 +125,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 // Port config
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
