70bbe7b9040e6ed22134778abf863a3df68b2af5
dnksebastian <sebastian.dulniawka@gmail.com>
2023-05-05 21:08:57 +0200

    phonebook backend 3.21

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
+  }
 
   next(error)
 }
@@ -45,13 +47,13 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 
   const body = request.body
 
-  if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({error: 'missing person data'})
-  }
+  // if (body.name === undefined || body.number === undefined) {
+  //   return response.status(400).json({error: 'missing person data'})
+  // }
 
   const person = new Person({
     name: body.name,
@@ -60,8 +62,7 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
-
+  }).catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -81,13 +82,15 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  // const body = request.body
+  const { name, number } = request.body
+
   const person = {
-    name: body.name,
-    number: body.number
+    name: name,
+    number: number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(updatedPerson => {
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' }).then(updatedPerson => {
     response.json(updatedPerson)
   }).catch(error => next(error))
 })
